335b0718d68d9a27e5af90f0d352e01bb0f262b7
OxBryte <silascyrax@gmail.com>
2025-07-26 14:35:58 +0100

    feat: update layout component styles, enhance dashboard with address truncation and UI elements

--- src/components/utils/utils.js
+++ src/components/utils/utils.js
@@ -7,7 +7,7 @@ export const truncateText = (text, maxLength, ellipsis = "...") => {
 };
 
 // Utility function to truncate an address (e.g., Ethereum address) for display
-export const truncateAddress = (address, startChars = 6, endChars = 4) => {
+export const truncateAddress = (address, startChars = 5, endChars = 4) => {
   if (!address) return "";
   if (address.length <= startChars + endChars) return address;
 
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import React from "react";
+import { truncateAddress } from "../components/utils/utils";
+import {
+  PiArrowCounterClockwise,
+  PiCopy,
+  PiGear,
+  PiStar,
+  PiStarFill,
+} from "react-icons/pi";
 
 export default function Dashboard() {
   return (
     <div className="w-full">
-      Dashboard
+      <div className="space-y-3 w-full">
+        <div className="flex gap-4 md:items-center flex-col md:flex-row w-full justify-between">
+          <div className="px-3 py-2 rounded-full border border-white/10 flex items-center gap-2.5">
+            <div className="w-2 h-2 rounded-full bg-green-400"></div>
+            <p className="text-[12px] font-semibold">
+              {truncateAddress("9oAmAmDbEzHfabwpSVgRAmW3Fm7sy1sNZvoQGAJwQ5r5")}
+            </p>
+            <div className="">
+              <PiCopy />
+            </div>
+            <div className="">
+              <PiStar />
+            </div>
+          </div>
+          <div className="flex items-center gap-3">
+            <div className="p-2 border border-white/10 rounded-full">
+              <PiArrowCounterClockwise />
+            </div>
+            <div className="p-2 border border-white/10 rounded-full">
+              <PiGear />
+            </div>
+            <input
+              type="text"
+              name=""
+              id=""
+              placeholder="Search address..."
+              className="py-2 px-4 placeholder:text-xs text-sm rounded-full bg-white/10 border-none"
+            />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
