ff8f746a151e88cff1a699ec319223a0b879dbf5
OxBryte <silascyrax@gmail.com>
2025-07-26 14:14:39 +0100

    feat: initialize base-watch project with React, Vite, and Tailwind CSS
    - Added package.json with dependencies for React, Vite, Tailwind CSS, and ESLint.
    - Created main application structure with App component and Dashboard page.
    - Integrated routing using react-router-dom.
    - Set up Tailwind CSS for styling and imported Inter Variable font.
    - Added SVG assets for Vite and React logos.
    - Configured Vite with Tailwind CSS and React plugin.
    - Created basic CSS files for global styles and component-specific styles.

---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Dashboard() {
+  return (
+    <div className="mx-auto w-full max-w-[calc(1112px+4rem)] px-5 mt-5 flex flex-col gap-7 pb-28 sm:mt-8 sm:pb-20">
+      Dashboard
+    </div>
+  );
+}
